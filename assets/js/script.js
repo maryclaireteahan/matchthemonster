1ad1de721de9edae9f2c410041b2d206ef8643cd
maryclaireteahan <maryclaireteahan@gmail.com>
2023-09-06 03:38:55 +0100

    feat: declare variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,22 @@
-// Get the elements from the HTML file
-const grid = document.querySelector(".grid");
-const cards = [
-    {image:"demon.png", value:"demon"},
-    {image:"zombie.png", value:"zombie"},
-    {image:"vampire.png", value:"vampire"},
-    {image:"witch.png", value:"witch"},
-    {image:"werewolf.png", value:"werewolf"},
-    {image:"ghost.png", value:"ghost"},
-    {image:"ogre.png", value:"ogre"},
-    {image:"dragon.png", value:"dragon"},
-];
-const cardsPicklist = [...cards, ...cards];
-const tileCount = cardsPicklist.length;
+const gridCOntainer = document.querySelector(".grid-container");
+const monsters = [];
 
-const button = document.querySelector(".btn");
-const instructionsButton = document.querySelector("#instructions-btn");
-const restartButton = document.querySelector("#restart-btn");
-const actions = document.querySelector(".actions");
-const scoreElement = document.querySelector(".score");
-const timeElement = document.querySelector(".time");
+// images in array
+
+monsters.push(['../images/demon.png','Demon']);
+monsters.push(['../images/zombie.png','Zombie']);
+monsters.push(['../images/witch.png','witch']);
+monsters.push(['../images/ghost.png','ghost']);
+monsters.push(['../images/werewolf.png','werewolf']);
+monsters.push(['../images/vampire.png','vampire']);
+monsters.push(['../images/ogre.png','ogre']);
+monsters.push(['../images/dragon.png','dragon']);
+
+const monstersPicklist = [...monsters, ...monsters];
+const tileCount = monstersPicklist.length;
+
+let revealedTileCount = 0;
+let activeTile = null;
+let awaitingEndOfMove = false;
+
+
